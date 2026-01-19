992b4ab22d69aa31e98ef243b952d6f922d10c70
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-19 16:13:21 +0530

    Initial Spring Boot Gradle project setup

---
+++ src/main/java/com/example/phoenixcodecrafterproject/PhoenixCodeCrafterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.phoenixcodecrafterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixCodeCrafterProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoenixCodeCrafterProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/phoenixcodecrafterproject/PhoenixCodeCrafterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phoenixcodecrafterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixCodeCrafterProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
