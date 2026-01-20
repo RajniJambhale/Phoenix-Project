185aa5c8e43efa3e2141d25012d5e8b2584d0644
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-20 15:55:49 +0530

    added validation for content and title

--- src/main/java/com/example/phoenixcodecrafterproject/model/Post.java
+++ src/main/java/com/example/phoenixcodecrafterproject/model/Post.java
@@ -3,6 +3,8 @@ package com.example.phoenixcodecrafterproject.model;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -19,9 +21,13 @@ public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private long id;
+
+    @NotBlank(message = "Title is required")
+    @Size(max = 200, message = "Title must not exceed 200 characters")
     private String title;
+
+    @NotBlank(message = "Content is required")
     private String content;
 
     @CreationTimestamp
