b3c281728b2ddeb516664a2d1e62b5ea5e96222d
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-20 10:04:53 +0530

    created post service

--- src/main/java/com/example/phoenixcodecrafterproject/service/PostService.java
+++ src/main/java/com/example/phoenixcodecrafterproject/service/PostService.java
@@ -1,13 +1,25 @@
 package com.example.phoenixcodecrafterproject.service;
-
 import com.example.phoenixcodecrafterproject.model.Post;
-
+import org.springframework.data.domain.Page;
 import java.util.List;
 
 public interface PostService {
     Post createPost(Post post);
+
     List<Post> getAllPosts();
+
     Post getPostById(long id);
+
     Post updatePost(long id, Post post);
+
     void deletePost(long id);
+
+    List<Post> getPostsByUser(Long userId);
+
+    List<Post> searchPosts(String keyword);
+
+    List<Post> postsLast7Days();
+
+    Page<Post> getAllPosts(int page, int size);
+
 }
