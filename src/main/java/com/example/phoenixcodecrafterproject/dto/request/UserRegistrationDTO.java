ad46980d0049e39423ee19167b7b8b2f946a4f0b
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-20 15:49:43 +0530

    created UserRegistrationDto

---
+++ src/main/java/com/example/phoenixcodecrafterproject/dto/request/UserRegistrationDTO.java
@@ -0,0 +1,27 @@
+package com.example.phoenixcodecrafterproject.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserRegistrationDTO {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Enter a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(
+            regexp = "^(?=.*\\d).+$",
+            message = "Password must contain at least one number"
+    )
+
+    private String password;
+}
