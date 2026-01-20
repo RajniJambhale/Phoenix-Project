de2e8378f9a6cba4b55179571a65e9e2315fbfaa
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-20 10:02:20 +0530

    added getpostbyuser, searchpost by keyword, getrecent post and getpostbypage

--- src/main/java/com/example/phoenixcodecrafterproject/controller/PostController.java
+++ src/main/java/com/example/phoenixcodecrafterproject/controller/PostController.java
@@ -2,6 +2,7 @@ package com.example.phoenixcodecrafterproject.controller;
 import com.example.phoenixcodecrafterproject.model.Post;
 import com.example.phoenixcodecrafterproject.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ public class PostController {
     @Autowired
     PostService postService;
 
+    // create post
     @PostMapping
     public ResponseEntity<Map<String, Object>> createPost(@RequestBody Post post) {
 
@@ -30,6 +32,7 @@ public class PostController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    // get  all post
     @GetMapping
     public ResponseEntity<List<Post>> getAllPosts() {
         List<Post> posts = postService.getAllPosts();
@@ -37,12 +40,14 @@ public class PostController {
     }
 
 
+    // get post by id
     @GetMapping("/{id}")
     public ResponseEntity<Post> getPostById(@PathVariable Long id) {
         Post post = postService.getPostById(id);
         return ResponseEntity.ok(post);
     }
 
+    // update post
     @PutMapping("/{id}")
     public ResponseEntity<Map<String, Object>> updatePost(
             @PathVariable Long id,
@@ -57,6 +62,7 @@ public class PostController {
         return ResponseEntity.ok(response);
     }
 
+    // delete post
     @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, Object>> deletePost(@PathVariable Long id){
         postService.deletePost(id);
@@ -64,6 +70,50 @@ public class PostController {
         Map<String,Object> response =  new HashMap<>();
         response.put("message", "Post deleted successfully with Id " + id);
         return ResponseEntity.ok(response);
+    }
+
+    // Find posts by user
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<Map<String, Object>> getPostsByUser(
+            @PathVariable Long userId) {
+
+        List<Post> userPosts = postService.getPostsByUser(userId);
+
+        Map<String, Object> response = new HashMap<>();
+
+        if (userPosts.isEmpty()) {
+            response.put("message", "Given user has not created any post yet");
+            response.put("posts", userPosts);
+            return ResponseEntity.ok(response);
+        }
+
+        response.put("message", "Posts fetched successfully");
+        response.put("posts", userPosts);
+
+        return ResponseEntity.ok(response);
+    }
+
+
+    //  Search posts by keyword
+    // GET /post/search?keyword=spring
+    @GetMapping("/search")
+    public List<Post> searchPosts(@RequestParam String keyword) {
+        return postService.searchPosts(keyword);
+    }
+
+    //  Posts from last 7 days
+    @GetMapping("/recent")
+    public List<Post> recentPosts() {
+        return postService.postsLast7Days();
+    }
+
+    //  Pagination
+    // GET /post?page=0&size=5
+    @GetMapping("/pages")
+    public Page<Post> getAllPosts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size) {
+        return postService.getAllPosts(page, size);
     }
 }
 
