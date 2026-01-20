016dae47dc1c6656fa3766964d235fc41b40318e
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-20 14:09:02 +0530

    declare getAllPost with Pageable

--- src/main/java/com/example/phoenixcodecrafterproject/service/PostService.java
+++ src/main/java/com/example/phoenixcodecrafterproject/service/PostService.java
@@ -1,6 +1,8 @@
 package com.example.phoenixcodecrafterproject.service;
 import com.example.phoenixcodecrafterproject.model.Post;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 public interface PostService {
@@ -20,6 +22,6 @@ public interface PostService {
 
     List<Post> postsLast7Days();
 
-    Page<Post> getAllPosts(int page, int size);
+    Page<Post> getAllPosts(Pageable pageable);
 
 }
