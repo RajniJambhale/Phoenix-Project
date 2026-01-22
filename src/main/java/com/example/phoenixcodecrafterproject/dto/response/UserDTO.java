c6b1ddaf0497ee0708b76ce5b4d1722f254cd6fd
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-22 12:04:28 +0530

    created user dto

---
+++ src/main/java/com/example/phoenixcodecrafterproject/dto/response/UserDTO.java
@@ -0,0 +1,17 @@
+package com.example.phoenixcodecrafterproject.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public class UserDTO {
+
+        private int id;
+        private String name;
+        private String email;
+}
