dabaa708cee15347709b418a6e03bd5bd02b9007
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-22 12:19:40 +0530

    refactor code

--- src/main/java/com/example/phoenixcodecrafterproject/repository/CommentRepository.java
+++ src/main/java/com/example/phoenixcodecrafterproject/repository/CommentRepository.java
@@ -3,8 +3,6 @@ import com.example.phoenixcodecrafterproject.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
--- src/main/java/com/example/phoenixcodecrafterproject/service/CommentService.java
+++ src/main/java/com/example/phoenixcodecrafterproject/service/CommentService.java
@@ -1,11 +1,7 @@
 package com.example.phoenixcodecrafterproject.service;
-
-
 import com.example.phoenixcodecrafterproject.model.Comment;
 
 public interface CommentService {
-
     Comment addComment(Long postId, Long userId, String content);
-
 }
 
