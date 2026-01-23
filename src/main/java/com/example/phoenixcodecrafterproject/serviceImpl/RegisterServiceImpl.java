ce2322ffd6db5e7c919302ee12b65b5d4aeb989f
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-23 16:55:39 +0530

    add class RegisterServiceImpl

---
+++ src/main/java/com/example/phoenixcodecrafterproject/serviceImpl/RegisterServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.phoenixcodecrafterproject.serviceImpl;
+
+import com.example.phoenixcodecrafterproject.dto.request.UserRegistrationDTO;
+import com.example.phoenixcodecrafterproject.exception.EmailAlreadyExistsException;
+import com.example.phoenixcodecrafterproject.model.User;
+import com.example.phoenixcodecrafterproject.repository.UserRepository;
+import com.example.phoenixcodecrafterproject.service.RegisterService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RegisterServiceImpl implements RegisterService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public RegisterServiceImpl(UserRepository userRepository,
+                               PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    @Override
+    public void register(UserRegistrationDTO dto) {
+
+        if (userRepository.existsByEmail(dto.getEmail())) {
+            throw new EmailAlreadyExistsException("Email already exists");
+        }
+
+        User user = new User();
+        user.setUsername(dto.getUsername());
+        user.setEmail(dto.getEmail());
+
+        // 🔐 Hash password (NOW WORKS)
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setRole("ROLE_USER");
+
+        userRepository.save(user);
+    }
+}
+
