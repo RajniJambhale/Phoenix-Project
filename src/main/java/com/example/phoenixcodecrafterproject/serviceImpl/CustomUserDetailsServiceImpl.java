c391b97e953f21ca937a97545160cf8a3612ffdb
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-23 16:54:54 +0530

    add class CustomUserDetailsServiceImpl

---
+++ src/main/java/com/example/phoenixcodecrafterproject/serviceImpl/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.phoenixcodecrafterproject.serviceImpl;
+
+import com.example.phoenixcodecrafterproject.exception.ResourceNotFoundException;
+import com.example.phoenixcodecrafterproject.model.User;
+import com.example.phoenixcodecrafterproject.repository.UserRepository;
+import com.example.phoenixcodecrafterproject.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsServiceImpl implements CustomUserDetailsService{
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(user.getRole())
+                .build();
+    }
+}
