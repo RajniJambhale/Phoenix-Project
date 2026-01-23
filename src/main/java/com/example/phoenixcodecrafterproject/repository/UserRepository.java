82f483a46db7697c74b3afa0361ac656450f6b7e
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-23 16:40:38 +0530

    change findbyemail to return optional

--- src/main/java/com/example/phoenixcodecrafterproject/repository/UserRepository.java
+++ src/main/java/com/example/phoenixcodecrafterproject/repository/UserRepository.java
@@ -2,13 +2,14 @@ package com.example.phoenixcodecrafterproject.repository;
 import com.example.phoenixcodecrafterproject.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.io.Serializable;
-import java.util.List;
+import java.util.Optional;
 
 
 @Repository
     public interface UserRepository extends JpaRepository<User, Serializable> {
-        List<User> findByEmail(String email);
-        boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 
 }
