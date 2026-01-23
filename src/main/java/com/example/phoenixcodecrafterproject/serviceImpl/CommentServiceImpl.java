05007959e97511742298a18d2fed55cba7791e38
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-23 16:49:18 +0530

    refactor code

--- src/main/java/com/example/phoenixcodecrafterproject/serviceImpl/CommentServiceImpl.java
+++ src/main/java/com/example/phoenixcodecrafterproject/serviceImpl/CommentServiceImpl.java
@@ -9,8 +9,6 @@ import com.example.phoenixcodecrafterproject.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
 public class CommentServiceImpl implements CommentService {
 
