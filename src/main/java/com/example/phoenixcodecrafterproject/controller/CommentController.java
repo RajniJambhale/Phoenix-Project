86024cf3aa05883eeffc19bd379da169c1729e24
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-29 10:02:37 +0530

    add crud opearation for comment

--- src/main/java/com/example/phoenixcodecrafterproject/controller/CommentController.java
+++ src/main/java/com/example/phoenixcodecrafterproject/controller/CommentController.java
@@ -1,21 +1,40 @@
 package com.example.phoenixcodecrafterproject.controller;
-import com.example.phoenixcodecrafterproject.model.Comment;
-import com.example.phoenixcodecrafterproject.dto.request.CommentRequest;
+
+import com.example.phoenixcodecrafterproject.dto.request.CreateCommentRequest;
+import com.example.phoenixcodecrafterproject.dto.request.UpdateCommentRequest;
+import com.example.phoenixcodecrafterproject.dto.response.CommentDTO;
 import com.example.phoenixcodecrafterproject.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/commets")
+@RequestMapping("/api")
 public class CommentController {
 
     @Autowired
     CommentService commentService;
 
-    @PostMapping("/{postId}")
-    public ResponseEntity<Comment> addComment(@PathVariable Long postId, @RequestBody CommentRequest request) {
-        Comment comment = commentService.addComment(postId, request.getUserId(), request.getContent());
-        return ResponseEntity.ok(comment);
+    @PostMapping("/comment/{postId}")
+    public ResponseEntity<CommentDTO> addComment(@RequestBody CreateCommentRequest request) {
+        return ResponseEntity.ok(commentService.addComment(request));
+    }
+
+    @GetMapping("/post/{postId}")
+    public ResponseEntity<List<CommentDTO>> getCommentByPostId(@PathVariable Long postId) {
+        return ResponseEntity.ok(commentService.getCommentByPostId(postId));
+    }
+
+    @PutMapping("/{postId}")
+    public ResponseEntity<CommentDTO> updateComment(@PathVariable Long id, @RequestBody UpdateCommentRequest request) {
+        return ResponseEntity.ok(commentService.updateComment(id, request));
+    }
+
+    @DeleteMapping("/{postIdd}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long id) {
+        commentService.deleteComment(id);
+        return ResponseEntity.noContent().build();
     }
 }
